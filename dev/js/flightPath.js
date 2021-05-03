cee27ef6838ae11bd17a3317ab47a091d687bc8a
peanvtman <sgj29@drexel.edu>
2021-05-03 18:52:09 -0400

    moon fades in

--- dev/js/flightPath.js
+++ dev/js/flightPath.js
@@ -9,13 +9,16 @@ export let flightPathTL = gsap.timeline();
 
 flightPathTL
 
-.to("#SpaceshipAlien",{duration:5, 
+.to("#SpaceshipAlien",{duration:7, 
     motionPath: {
         path: "#motionPath",
         align: "#motionPath"
     },
 
     ease: "power4.out"
-});
+}, "moonLand")
 
+.fromTo("#Moon", {display:"block", scale: .25, alpha:0, x:50, y:50}, {scale: 10, duration: 3.5, x:"-=300", y:"+=500", alpha:1, ease: "none"}, "moonLand")
+
+;
 // MotionPathHelper.create("#SpaceshipAlien")
