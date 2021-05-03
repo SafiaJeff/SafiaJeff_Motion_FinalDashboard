9d89fe9ff771371c994fe514b63094b5d014516b
peanvtman <sgj29@drexel.edu>
2021-05-03 19:03:59 -0400

    ship lands - star questions

--- dev/js/flightPath.js
+++ dev/js/flightPath.js
@@ -6,6 +6,8 @@ gsap.registerPlugin(MotionPathPlugin, MotionPathHelper);
 
 export let flightPathTL = gsap.timeline();
 
+gsap.set("#Stars path",{transformOrigin:"center center"});
+
 
 flightPathTL
 
@@ -19,6 +21,10 @@ flightPathTL
 }, "moonLand")
 
 .fromTo("#Moon", {display:"block", scale: .25, alpha:0, x:50, y:50}, {scale: 10, duration: 3.5, x:"-=300", y:"+=500", alpha:1, ease: "none"}, "moonLand")
-
+.fromTo("#Stars", {display:"block", alpha:0}, {duration: 3.5, alpha:1, ease: "none"}, "moonLand")
+// .fromTo("#Stars path", {display:"block", alpha:0, stagger:1}, {duration: 3.5, alpha:1, ease: "none"}, "moonLand")
+//stars will disappear if using code above. also z-index messed up
 ;
+
+
 // MotionPathHelper.create("#SpaceshipAlien")
