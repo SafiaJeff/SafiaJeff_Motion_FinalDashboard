604e6291c3cd2720bdd38cadadf672a0bc30819b
peanvtman <sgj29@drexel.edu>
2021-05-04 12:55:18 -0400

    moon winks

--- dev/js/zoomOut.js
+++ dev/js/zoomOut.js
@@ -6,6 +6,8 @@ gsap.registerPlugin(MotionPathPlugin, MotionPathHelper);
 
 export let zoomOutTL = gsap.timeline();
 
+gsap.set("#moon-left-eye",{transformOrigin:"center center"});
+
 zoomOutTL
 
 .to("#SpaceshipAlien",{duration:3, 
@@ -17,7 +19,10 @@ zoomOutTL
     ease: "power1.inOut", scale: .5
 }, "byemooncow")
 
-.to("#Moon", {scale: 6, x:"10", y:"-=300", duration: 3, ease: "power1.inOut"}, "byemooncow")
+.to("#Moon", {scale: 6, x:"10", y:"-=400", duration: 3, ease: "power1.inOut"}, "byemooncow")
+.to("#CowCopy", {scale: 0, x:"300", y:"-=550", duration: 3, ease: "power1.inOut"}, "byemooncow")
+.to("#moon-left-eye", {scaleY:.15, duration:.18, delay:.5})
+.to("#moon-left-eye", {scaleY:1, duration:.18})
 
 ;
 
