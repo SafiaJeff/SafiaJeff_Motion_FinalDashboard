c9d6531a93f28d504ab3dc374e0077d46ea609da
peanvtman <sgj29@drexel.edu>
2021-05-04 12:17:44 -0400

    added cowcopy & spit out on moon

--- dev/js/flightPath.js
+++ dev/js/flightPath.js
@@ -11,7 +11,7 @@ gsap.set("#Stars path",{transformOrigin:"center center"});
 
 flightPathTL
 
-.to("#SpaceshipAlien",{duration:7, 
+.to("#SpaceshipAlien",{duration:6, 
     motionPath: {
         path: "#motionPath",
         align: "#motionPath"
@@ -20,7 +20,7 @@ flightPathTL
     ease: "power4.out"
 }, "moonLand")
 
-.fromTo("#Moon", {display:"block", scale: .25, alpha:0, x:50, y:50}, {scale: 10, duration: 3.5, x:"-=300", y:"+=500", alpha:1, ease: "none"}, "moonLand")
+.fromTo("#Moon", {display:"block", scale: 0, x:50, y:50}, {scale: 10, duration: 3.5, x:"-=300", y:"+=500", alpha:1, ease: "none"}, "moonLand")
 .fromTo("#Stars", {display:"block", alpha:0}, {duration: 3.5, alpha:1, ease: "none"}, "moonLand")
 // .fromTo("#Stars path", {display:"block", alpha:0, stagger:1}, {duration: 3.5, alpha:1, ease: "none"}, "moonLand")
 //stars will disappear if using code above. also z-index messed up
