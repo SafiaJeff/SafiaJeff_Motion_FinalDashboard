69a08c0bfb5379ae7a89a35ea42bb70abd2f6731
peanvtman <sgj29@drexel.edu>
2021-05-06 18:34:31 -0400

    revised flight path

--- dev/js/cowSuck.js
+++ dev/js/cowSuck.js
@@ -16,6 +16,7 @@ cowSuckTL
 .to("#SpaceshipAlien",{x: "-=180", duration: .5, ease:"none"}, "-=2")
 .to("#alienEnergy path",{duration: 2, stagger: .25, display:"block", scale:1, ease: "elastic.out(1, 0.2)"}, "-=.75")
 .to("#Cow",{duration:4, scale:.5, opacity:0, rotate: 360, x:"+=100", y:"-=200"}, "-=.75")
+// .to("#SpaceshipAlien",{x: "+=250", duration: .5, ease:"none"}, "-=.5")
 .to("#alienEnergy path",{duration: .5, stagger: .25, display:"none", scale:0}, "-=1")
 .to("#SpaceshipAlien",{duration:3, y:"+=300", ease: "elastic.out(1, 0.5)"})
 
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -29,4 +29,4 @@ mainTL.add(zoomOutTL);
 
 
 // DEV Tools
-GSDevTools.create();
+// GSDevTools.create();
