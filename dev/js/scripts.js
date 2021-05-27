0c999bf9ebc084d901581cdc85a759d0b8959280
peanvtman <sgj29@drexel.edu>
2021-05-27 15:19:38 -0400

    cleaned out copied code

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,27 +5,13 @@ import { GSDevTools } from "gsap/GSDevTools";
 gsap.registerPlugin(GSDevTools);
 
 // My Imports
-import { zoomTL } from "./zoomIn";
-import { midAppearTL } from "./midAppear";
-import { skyStartTL } from "./skyStart";
-import { cowSuckTL } from "./cowSuck";
-import { blastOffTL } from "./blastOff";
-import { flightPathTL } from "./flightPath";
-import { cowSpitTL } from "./cowSpit";
-import { zoomOutTL } from "./zoomOut";
+// import { zoomTL } from "./zoomIn";
 
 
 // Main Timeline
 let mainTL = gsap.timeline();
 
-mainTL.add(zoomTL);
-mainTL.add(midAppearTL, "-=2.5");
-mainTL.add(skyStartTL, "-=3.5");
-mainTL.add(cowSuckTL, "-=1");
-mainTL.add(blastOffTL, "-=1.5");
-mainTL.add(flightPathTL, "-=1.5");
-mainTL.add(cowSpitTL);
-mainTL.add(zoomOutTL);
+// mainTL.add(zoomTL);
 
 
 // DEV Tools
