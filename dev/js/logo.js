dff4d42e3b0c44c85e97e5ff21ae487dff828d3b
peanvtman <sgj29@drexel.edu>
2021-06-01 18:09:11 -0400

    ghost/stars explode out

--- dev/js/logo.js
+++ dev/js/logo.js
@@ -1,6 +1,7 @@
 import { gsap } from "gsap";
 export let logoTL = gsap.timeline();
 
+gsap.set("#ghostText",{transformOrigin:"center center"});
 
 logoTL
 //RR scales down
@@ -9,5 +10,10 @@ logoTL
 .to("#RRLogo",{scale:0, duration:2, alpha:0, ease: "expo.in"}, "-=2")
 
 //Ghost & stars explode out
+.fromTo("#ghostText",{display:"block", alpha:0, scale:0},{scale:1, alpha:1,duration:3, ease: "expo.out"}, "explode")
+.to("#smallStars",{scale:1.25, duration:2.75, alpha:1, ease: "expo.out"}, "explode")
+.to("#medStars",{duration:3, scale:1.5, alpha:1, ease: "expo.out"}, "explode")
+
+//Everything fades out
 
 ;
