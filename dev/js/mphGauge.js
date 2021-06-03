d741e1e8d412f7945f274f39706cdf53de167a05
peanvtman <sgj29@drexel.edu>
2021-06-03 16:41:32 -0400

    tick marks

--- dev/js/mphGauge.js
+++ dev/js/mphGauge.js
@@ -2,7 +2,13 @@ import { gsap } from "gsap";
 export let mphEntryTL = gsap.timeline();
 
 gsap.set("#mphInnerCircle", { transformOrigin: "center center", rotate:"90" });
+// gsap.set("#mphPath2", { transformOrigin: "center center", scaleY: -1});
+// gsap.set("#mphPath4", { transformOrigin: "center center", scaleX: -1, scaleY: -1});
 
 mphEntryTL
+//circles
 .from("#mphInnerCircle",{ duration: 1.5, drawSVG:0 }, "mph")
 .from("#mphOuterCircle path",{alpha:0, duration: 1.75, drawSVG:0}, "mph")
+
+//tick marks
+.from("#mphGaugeLines path",{ duration: 1, drawSVG:0 })
