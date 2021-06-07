94efd4f27e13462ffa0f153a7216c4e4597001a7
peanvtman <sgj29@drexel.edu>
2021-06-07 15:51:24 -0400

    album cover inserted

--- dev/js/musicGauge.js
+++ dev/js/musicGauge.js
@@ -12,7 +12,7 @@ musicEntryTL
 .from("#musicOuterCircle path",{alpha:0, duration: 1.75, drawSVG:0}, "music")
 
 //photo & icons
-.from("#albumPhotoMask",{ duration: 1.5, alpha:0}, "-=.75")
+.from("#simonDominicAlbum",{ duration: 1.5, alpha:0}, "-=.75")
 .from("#musicIcons",{ duration: .75, y:"-=20", alpha:0}, "-=1")
 
 
