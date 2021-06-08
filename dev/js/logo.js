2c3920982190f98f48687b738d67c5c6c35f3e99
peanvtman <sgj29@drexel.edu>
2021-06-08 11:33:11 -0400

    main timeline timing tweaks

--- dev/js/logo.js
+++ dev/js/logo.js
@@ -10,7 +10,7 @@ logoTL
 //RR scales down
 .to("#smallStars",{scale:.25, duration:.75, alpha:.25, ease: "expo.in"})
 .to("#medStars",{duration:1, scale:.5, alpha:0, ease: "expo.in"}, "-=1")
-.to("#RRLogo",{scale:0, duration:1, alpha:0, ease: "expo.in"}, "-=1")
+.to("#RRLogo",{scale:0, duration:1, alpha:0, ease: "expo.in"}, "-=.75")
 
 //Ghost & stars explode out
 .fromTo("#ghostText",{display:"block", alpha:0, scale:0},{scale:1, alpha:1,duration:.75, ease: "slow(0.7, 0.7, false)"}, "explode")
